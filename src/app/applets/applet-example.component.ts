4bf53046eecb16541567bf20ef57f30052bc812e
Jacob Chappell <chappellind@gmail.com>
2019-02-11 21:35:41 -0500

    Add basic example applet animation.

--- src/app/applets/applet-example.component.ts
+++ src/app/applets/applet-example.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer } from '@angular/core';
 import { Applet } from './applet';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'applet-example',
   templateUrl: './applet.html',
   styleUrls: ['./applet.scss']
 })
-export class AppletExampleComponent extends Applet implements OnInit {
+export class AppletExampleComponent extends Applet implements OnInit, AfterViewInit {
   private color;
+  private angle;
 
-  constructor() {
-    super();
+  constructor(protected platform: Platform, protected renderer: Renderer) {
+    super(platform, renderer, true);
+    this.angle = 0;
   }
 
   ngOnInit() {
@@ -20,4 +23,27 @@ export class AppletExampleComponent extends Applet implements OnInit {
     }
   }
 
+  ngAfterViewInit() {
+    super.ngAfterViewInit();
+  }
+
+  protected draw() {
+    super.draw();
+    let small_length = (this.canvas.width < this.canvas.height) ? this.canvas.width : this.canvas.height;
+    let radius = small_length * 0.25;
+    let delta = radius * 0.002;
+    this.ctx.lineWidth = radius * 0.05;
+    this.ctx.strokeStyle = this.color;
+    for (let i = 0; i < 4; ++i) {
+      this.ctx.beginPath();
+      this.ctx.arc(this.canvas.width / 2,
+                  this.canvas.height / 2,
+                  radius,
+                  this.angle + (Math.PI / 2) * i + delta,
+                  this.angle + (Math.PI / 2) * (i + 1) - delta,
+                  false);
+      this.ctx.stroke();
+    }
+    this.angle += 0.025;
+  }
 }
--- src/app/applets/applet.ts
+++ src/app/applets/applet.ts
@@ -1,11 +1,44 @@
-export class Applet {
+import { ViewChild, ElementRef, Renderer, AfterViewInit } from "@angular/core";
+import { Platform } from "@ionic/angular";
+
+export class Applet implements AfterViewInit {
+    @ViewChild('canvas') canvasRef: ElementRef;
+    protected canvas: HTMLCanvasElement;
+    protected ctx: CanvasRenderingContext2D;
     protected appletTag: HTMLElement;
 
-    constructor() {
+    constructor(protected platform: Platform, protected renderer: Renderer, private animated: boolean) {
         console.log('Applet API constructor called.');
+    }
+
+    ngAfterViewInit() {
+        this.canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext('2d');
+        this.resize();
+        if (this.animated) {
+            setInterval(() => {
+                this.draw();
+            }, 33);
+        } else {
+            this.draw();
+        }
     }
 
     setAppletTag(tag: HTMLElement) {
         this.appletTag = tag;
+    }
+
+    protected resize() {
+        this.renderer.setElementAttribute(this.canvas, 'width', (this.platform.width() * 2) + '');
+        this.renderer.setElementAttribute(this.canvas, 'height', (this.platform.height() * 2) + '');
+    }
+
+    protected clear() {
+        this.ctx.fillStyle = '#EEEEEE';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    protected draw() {
+        this.clear();
     }
 }
