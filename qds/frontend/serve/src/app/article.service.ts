b1b2ada82adbe8c08a34709f146b54b8c5d0bd31
Jacob Chappell <jacob.chappell@uky.edu>
2019-02-19 22:50:13 -0500

    Update proxyconfig, fix path bug.

--- qds/frontend/serve/src/app/article.service.ts
+++ qds/frontend/serve/src/app/article.service.ts
@@ -7,8 +7,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class ArticleService {
   //readonly urlPrefix = 'http://127.0.0.1:5000';
-  // Use empty urlPrefix when using proxyconfig.json.
-  readonly urlPrefix = '';
+  readonly urlPrefix = '/api';
   articlesBehaviorSubject: BehaviorSubject<Array<any>> = null;
   articlesObserver: Observable<Array<any>> = null;
   articles: Array<any> = null;
